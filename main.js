f05f82e125d180a7a7d76a37be0afbbcd091ec4b
ldlldlsa <mmmmostafa0011@gmail.com>
2025-10-29 19:27:34 +0300

    second update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 new TypeIt("#myElement", {
     strings: ['Interior design ' , 'Furniture Designer'],
-    speed: 150,
+    speed: 250,
     breakLines: false,
     waitUntilVisible: true ,
   
@@ -12,3 +12,24 @@ cursorChar: '|',
 
 
 }).go();
+
+
+        // تهيئة EmailJS بالـ Public Key
+        emailjs.init("lCRpm9pw1ATgW40cF"); // ضع Public Key هنا
+
+        document.getElementById('contact-form').addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            // إرسال النموذج
+            emailjs.sendForm('service_gm51enl', 'template_65wiypr', this)
+                .then(function() {
+                    alert('تم إرسال الرسالة بنجاح!');
+                }, function(error) {
+                    alert('فشل الإرسال: ' + error.text);
+                });
+              
+        });
+
+
+
+
